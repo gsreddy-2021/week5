6bcf020dceb03677e1d292eca44a2459df1807a0
S Reddy <sachitgreddy@email.com>
2021-02-04 20:00:24 -0500

    Step 1 completed

--- practice/1-dom.js
+++ practice/1-dom.js
@@ -4,11 +4,21 @@ function getRandomInt(max) {
 
 window.addEventListener('DOMContentLoaded', function () {
 
-  // 1. use getRandomInt(6) to randomize dice values
-  // 2. find the dice element
+  
+  
   // 3. replace the element's inner html with new html using the dice values above
 
   // ⬇️ ⬇️ ⬇️ YOUR CODE GOES HERE ⬇️ ⬇️ ⬇️
+
+  // 1. use getRandomInt(6) to randomize dice values
+  let die1 = getRandomInt(6)
+  let die2 = getRandomInt(6)
+
+  // 2. find the dice element
+
+
+
+
 
   // ⬆️ ⬆️ ⬆️ YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE ⬆️ ⬆️ ⬆️
 })
