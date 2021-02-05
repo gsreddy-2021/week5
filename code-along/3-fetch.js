3fc327ca331c26599c92b648e5b66d403585831d
S Reddy <sachitgreddy@email.com>
2021-02-04 21:19:04 -0500

    completed the fetch file task

--- code-along/3-fetch.js
+++ code-along/3-fetch.js
@@ -7,4 +7,23 @@
 
 window.addEventListener('DOMContentLoaded', function () {
   // let url = 'https://api.coindesk.com/v1/bpi/currentprice/USD.json'
+  let form = document.querySelector('.bitcoin-form')
+  form.addEventListener('submit', async function(event) { //await will return error if there is no async
+    event.preventDefault()
+    console.log('submitted')
+
+    //let response = fetch('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
+    let response = await fetch('https://api.coindesk.com/v1/bpi/currentprice/USD.json')
+    //console.log(response)
+    let json = await response.json()
+    console.log(json)
+
+    let amount = document.querySelector('#amount').value
+    let rate = json.bpi.USD.rate_float
+    let convertedAmount = amount * rate
+
+    let outputElement = document.querySelector('.output')
+    outputElement.innerHTML = `Your ${amount} Bitcoin is worth ${convertedAmount} USD`
+  })
+
 })
