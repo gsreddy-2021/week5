3ec864b5355d12ed27a9921366e6ad79ad593900
S Reddy <sachitgreddy@email.com>
2021-02-04 20:01:46 -0500

    Completed Step 2

--- practice/1-dom.js
+++ practice/1-dom.js
@@ -5,9 +5,6 @@ function getRandomInt(max) {
 window.addEventListener('DOMContentLoaded', function () {
 
   
-  
-  // 3. replace the element's inner html with new html using the dice values above
-
   // ⬇️ ⬇️ ⬇️ YOUR CODE GOES HERE ⬇️ ⬇️ ⬇️
 
   // 1. use getRandomInt(6) to randomize dice values
@@ -15,8 +12,11 @@ window.addEventListener('DOMContentLoaded', function () {
   let die2 = getRandomInt(6)
 
   // 2. find the dice element
+  let dice = document.querySelector('.dice')
+  console.log(dice)
 
-
+  // 3. replace the element's inner html with new html using the dice values above
+  
 
 
 
