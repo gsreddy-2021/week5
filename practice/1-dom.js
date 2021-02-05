806142fa16f49f3b15a522937475a8626554917a
S Reddy <sachitgreddy@email.com>
2021-02-04 20:04:02 -0500

    completed step 3

--- practice/1-dom.js
+++ practice/1-dom.js
@@ -16,9 +16,9 @@ window.addEventListener('DOMContentLoaded', function () {
   console.log(dice)
 
   // 3. replace the element's inner html with new html using the dice values above
-  
-
-
-
+  dice.innerHTML =  `
+        <img src="../images/dice/${die1}.png" class="die w-1/2 md:w-1/6">
+        <img src="../images/dice/${die2}.png" class="die w-1/2 md:w-1/6">
+        `
   // ⬆️ ⬆️ ⬆️ YOUR CODE ENDS HERE. DON'T CHANGE ANY OTHER CODE ⬆️ ⬆️ ⬆️
 })
